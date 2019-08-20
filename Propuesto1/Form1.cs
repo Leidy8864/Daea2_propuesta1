99c6befe1d71b3e3a8cd9c93596a6d0912616397
Leidy C_S <leidy.callupe@tecsup.edu.pe>
2019-08-19 20:41:20 -0500

    first commit

---
+++ Propuesto1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Propuesto1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["kobu"].ConnectionString);
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaPedidosNombre", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@name", txtName.Text);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Pedidos");
+                        dgPedidos.DataSource = df.Tables["Pedidos"];
+                    }
+                }
+            }
+        }
+
+        private void dgPedidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
